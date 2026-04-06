e7ee2308c2668120c10f59813d4a89b19d9d2e95
Komal-coder-maker <kr730936@gmail.com>
2026-04-06 15:29:25 +0530

    fix:all pages are updated

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'tictactoe-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install event - caching assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activate event - cleaning old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
+
+// Fetch event - needed for PWA install prompt
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
